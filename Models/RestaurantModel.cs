b590a28b4661354a977380efe80db67a6c601c86
ThiagoLG <thiago.lg53@gmail.com>
2023-03-16 21:11:57 -0300

    Criação de modelos + inclusão de arquivos csv

---
+++ Models/RestaurantModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantsFinder.Models
+{
+  internal class RestaurantModel
+  {
+    public string? Name { get; set; }
+    public int? Rating { get; set; }
+    public int? Distance { get; set; }
+    public int? KitchenId { get; set; }
+    public KitchenModel? Kitchen { get; set; }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+class KitchenFinder
+{
+  private static readonly string kitchensPath = "./Files/kitchens.csv";
+  private static readonly string restaurantsPath = "./Files/restaurants.csv";
+
+  public static void Main(string[] args)
+  {
+
+    var kitchensReader = new StreamReader(File.OpenRead(kitchensPath));
+    var restaurantsReader = new StreamReader(File.OpenRead(restaurantsPath));
+
+     
+
+  }
+
+}
